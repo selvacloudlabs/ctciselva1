b2bbdf601af815591430e2ad973889b1241797ab
selvacloudlabs <selvacloudlabs@gmail.com>
2021-09-07 17:41:16 +0000

    first commit

---
+++ arrays.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+// this is a comment
+/*
+func main() {
+fmt.Println("Hello, World")
+}
+*/
+func main() {
+	fmt.Println(len("Hello, World"))
+        fmt.Println("HAallo, World"[1])
+        fmt.Println("Hello, " + "World")
+
+}
+
