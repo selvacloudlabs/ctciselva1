8a7e75a518a5a11e67bfa2a26a8d2ea139c996c5
selvacloudlabs <selvacloudlabs@gmail.com>
2021-09-10 10:04:05 +0000

    linked list

--- linked.go
+++ linked.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"fmt"
@@ -10,117 +10,186 @@ type Node struct {
 	next  *Node
 }
 
-// LinkedList represents a linked list
-type LinkedList struct {
-	head *Node
-	len  int
-}
+var listHead *Node
 
+// Insert inserts new node at the end of  from linked list
+func insertNode(head *Node, val int) {
 
-// Search returns node position with given value from linked list
-func (l *LinkedList) Search(val int) int {
-	ptr := l.head
-	for i := 0; i < l.len; i++ {
-		if ptr.value == val {
-			return i
+	newNode := Node{}
+	newNode.value = val
+	newNode.next = nil
+
+	if head == nil {
+		listHead = &newNode
+		return
+	}
+
+	cur := head
+
+	for cur != nil {
+
+		if cur.next == nil {
+			cur.next = &newNode
+			return
 		}
-		ptr = ptr.next
+		cur = cur.next
 	}
-	return -1
+
+	return
 }
 
+// Delete inserts new node at the end of  from linked list
+func deleteNode(head *Node, val int) {
 
-// Print displays all the nodes from linked list
-func (l *LinkedList) Print() {
-	if l.len == 0 {
-		fmt.Println("No nodes in list")
+	if head == nil {
+		fmt.Println("Head NULL")
+		return
 	}
-	ptr := l.head
-	for i := 0; i < l.len; i++ {
-		fmt.Println("Node: ", ptr)
-		ptr = ptr.next
+
+	prev := head
+	cur := head
+
+	for cur != nil {
+
+		if cur.value == val {
+			if listHead == cur {
+				listHead = cur.next
+			} else {
+				prev.next = cur.next
+			}
+		}
+
+		prev = cur
+		cur = cur.next
 	}
+
+	return
 }
 
 // Insert inserts new node at the end of  from linked list
-func (l *LinkedList) Insert(val int) {
-	n := Node{}
-	n.value = val
-	if l.len == 0 {
-		l.head = &n
-		l.len++
+func printNode(head *Node) {
+
+	if head == nil {
+		fmt.Println("Head NULL")
 		return
 	}
-	ptr := l.head
-	for i := 0; i < l.len; i++ {
-		if ptr.next == nil {
-			ptr.next = &n
-			l.len++
-			return
+	fmt.Println("------------------ Printing LIst ----------------")
+
+	cur := head
+
+	for cur != nil {
+		fmt.Println("Node : ", cur)
+		cur = cur.next
+	}
+
+	return
+}
+
+// 2.1 Remove Dups: Write code to remove duplicates from an unsorted linked list.
+// Method 2
+// space complexity - O(n)
+// time complexity - O(n)
+/**
+ * [removeDuplicates1 - Remove duplicates from the list using hash table]
+ * @param head [head of list]
+ */
+
+func removeDuplicates(head *Node) {
+	//if ( head == nil ||  (head && (head.next == nil ))) {
+	if head == nil {
+		return
+	}
+	foundHashTable := make(map[int]bool)
+	prev := head
+	cur := head.next
+
+	foundHashTable[head.value] = true
+
+	for cur != nil {
+		if foundHashTable[cur.value] {
+			prev.next = cur.next
+		} else {
+			foundHashTable[cur.value] = true
+			prev = cur
 		}
-		ptr = ptr.next
+		cur = cur.next
 	}
 }
 
-// GetAt returns node at given position from linked list
-func (l *LinkedList) GetAt(pos int) *Node {
-	ptr := l.head
-	if pos < 0 {
-		return ptr
+/*
+2.2 Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
+*/
+func kthToLast(head *Node, k int) *Node {
+	p1 := head
+	p2 := head
+
+	/* Move p1 k nodes into the list.*/
+	for i := 0; i < k; i++ {
+		if p1 == nil {
+			return nil
+		} // Out of bounds
+		p1 = p1.next
 	}
-	if pos > (l.len - 1) {
-		return nil
+
+	/* Move them at the same pace. When p1 hits the end,
+	 * p2 will be at the right element. */
+	for p1 != nil {
+		p1 = p1.next
+		p2 = p2.next
 	}
-	for i := 0; i < pos; i++ {
-		ptr = ptr.next
-	}
-	return ptr
+	return p2
 }
 
+/*
+2.3 Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but
+the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+that node.
+EXAMPLE
+lnput:the node c from the linked list a->b->c->d->e->f
+Result: nothing is returned, but the new linked list looks like a ->b->d- >e- >f
+*/
+func deleteMiddleNode(midNode *Node) bool {
 
-// DeleteVal deletes node having given value from linked list
-func (l *LinkedList) DeleteVal(val int) bool {
-	ptr := l.head
-	if l.len == 0 {
-		fmt.Println("List is empty")
-		return false
+	if midNode == nil || midNode.next == nil {
+		return false // Failure
 	}
-	for i := 0; i < l.len; i++ {
-		if ptr.value == val {
-			if i > 0 {
-				prevNode := l.GetAt(i - 1)
-				prevNode.next = l.GetAt(i).next
-			} else {
-				l.head = ptr.next
-			}
-			l.len--
-			return true
-		}
-		ptr = ptr.next
-	}
-	fmt.Println("Node not found")
-	return  false
-
-
+	nextNode := midNode.next
+	midNode.value = nextNode.value
+	midNode.next = nextNode.next
+	return true
 }
 
-func main () {
+func main() {
 
-    listA := LinkedList{}
+	//head := Node{}
 
-    listA.Print()
-    listA.Insert(5)
-    listA.Print()
-    listA.Insert(6)
-    listA.Print()
+	//head.value = 4
+	//printNode(listHead)
+	insertNode(listHead, 4)
 
-   listA.Insert(7)
-    listA.Print()
+	printNode(listHead)
 
-fmt.Println(listA.DeleteVal(8))
-//listA.DeleteVal(8)
-listA.Print()
+	insertNode(listHead, 6)
+	insertNode(listHead, 6)
+	insertNode(listHead, 8)
+	insertNode(listHead, 9)
+	insertNode(listHead, 13)
+	insertNode(listHead, 14)
+	insertNode(listHead, 25)
+	printNode(listHead)
+	removeDuplicates(listHead)
+	printNode(listHead)
+	/*
+		deleteNode(listHead, 4)
+		printNode(listHead)
+		deleteNode(listHead, 6)
+		printNode(listHead)
+		deleteNode(listHead, 8)
+		printNode(listHead)
+	*/
+	fmt.Println(kthToLast(listHead, 3))
 
-
+	deleteMiddleNode(kthToLast(listHead, 3))
+	printNode(listHead)
 
 }
