9e74fad4b0fb4e5307aacc1c517e86df4a9ae521
selvacloudlabs <selvacloudlabs@gmail.com>
2021-09-10 15:56:23 +0000

    list complete

--- linked.go
+++ linked.go
@@ -10,18 +10,16 @@ type Node struct {
 	next  *Node
 }
 
-var listHead *Node
-
 // Insert inserts new node at the end of  from linked list
-func insertNode(head *Node, val int) {
+func insertNode(head *Node, val int) *Node {
 
-	newNode := Node{}
+	newNode := new(Node)
 	newNode.value = val
 	newNode.next = nil
 
 	if head == nil {
-		listHead = &newNode
-		return
+		head = newNode
+		return head
 	}
 
 	cur := head
@@ -29,21 +27,22 @@ func insertNode(head *Node, val int) {
 	for cur != nil {
 
 		if cur.next == nil {
-			cur.next = &newNode
-			return
+			cur.next = newNode
+			return head
 		}
 		cur = cur.next
 	}
 
-	return
+	return head
 }
 
 // Delete inserts new node at the end of  from linked list
-func deleteNode(head *Node, val int) {
+
+func deleteNode(head *Node, val int) *Node {
 
 	if head == nil {
 		fmt.Println("Head NULL")
-		return
+		return nil
 	}
 
 	prev := head
@@ -52,8 +51,8 @@ func deleteNode(head *Node, val int) {
 	for cur != nil {
 
 		if cur.value == val {
-			if listHead == cur {
-				listHead = cur.next
+			if head == cur {
+				head = cur.next
 			} else {
 				prev.next = cur.next
 			}
@@ -63,7 +62,7 @@ func deleteNode(head *Node, val int) {
 		cur = cur.next
 	}
 
-	return
+	return head
 }
 
 // Insert inserts new node at the end of  from linked list
@@ -159,37 +158,121 @@ func deleteMiddleNode(midNode *Node) bool {
 	return true
 }
 
+/*
+2.4 Partition: Write code to partition a linked list around a value x, such that all nodes less than x come
+before all nodes greater than or equal to x. If x is contained within the list the values of x only need
+to be after the elements less than x (see below). The partition element x can appear anywhere in the
+"right partition"; it does not need to appear between the left and right partitions.
+EXAMPLE
+Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition= 5]
+Output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+*/
+
+func partitionList(node *Node, x int) *Node {
+
+	head := node
+	tail := node
+
+	printNode(node)
+
+	/* Partition list */
+	for node != nil {
+		/* Prserve next before changing */
+		next := node.next
+		if node.value < x {
+			/* Insert node at head. */
+			node.next = head
+			head = node
+		} else {
+			/* Insert node at tail. */
+			tail.next = node
+			tail = node
+		}
+		node = next
+	}
+	tail.next = nil
+
+	printNode(head)
+
+	return head
+}
+
+/*
+2.5 Sum Lists: You have two numbers represented by a linked list, where each node contains a single
+digit. The digits are stored in reverse order, such that the 1 's digit is at the head of the list. Write a
+function that adds the two numbers and returns the sum as a linked list.
+EXAMPLE
+Input: (7-> 1 -> 6) + (5 -> 9 -> 2).That is,617 + 295.
+Output: 2 -> 1 -> 9. That is, 912.
+*/
+func addLists(l1 *Node, l2 *Node, carry int) *Node {
+
+	if l1 == nil && l2 == nil && carry == 0 {
+		return nil
+	}
+
+	resultNode := new(Node)
+	value := carry
+
+	if l1 != nil {
+		value += l1.value
+	}
+	if l2 != nil {
+		value += l2.value
+	}
+	resultNode.value = value % 10
+	if l1 != nil || l2 != nil {
+		if value >= 10 {
+			resultNode.next = addLists(l1.next, l2.next, 1)
+		} else {
+			resultNode.next = addLists(l1.next, l2.next, 0)
+		}
+	}
+	return resultNode
+}
+
 func main() {
 
 	//head := Node{}
+	var listHead *Node
 
-	//head.value = 4
+	listHead = insertNode(listHead, 7)
+	listHead = insertNode(listHead, 1)
+	listHead = insertNode(listHead, 6)
+	listHead = insertNode(listHead, 6)
+	listHead = insertNode(listHead, 5)
+	listHead = insertNode(listHead, 2)
+	printNode(listHead)
+	//removeDuplicates(listHead)
 	//printNode(listHead)
-	insertNode(listHead, 4)
-
-	printNode(listHead)
-
-	insertNode(listHead, 6)
-	insertNode(listHead, 6)
-	insertNode(listHead, 8)
-	insertNode(listHead, 9)
-	insertNode(listHead, 13)
-	insertNode(listHead, 14)
-	insertNode(listHead, 25)
-	printNode(listHead)
-	removeDuplicates(listHead)
-	printNode(listHead)
 	/*
-		deleteNode(listHead, 4)
+		listHead = deleteNode(listHead, 9)
 		printNode(listHead)
-		deleteNode(listHead, 6)
+		listHead = deleteNode(listHead, 6)
 		printNode(listHead)
-		deleteNode(listHead, 8)
+		listHead = deleteNode(listHead, 8)
 		printNode(listHead)
 	*/
 	fmt.Println(kthToLast(listHead, 3))
 
 	deleteMiddleNode(kthToLast(listHead, 3))
 	printNode(listHead)
+	listHead = partitionList(listHead, 5)
+	printNode(listHead)
 
+	/* addLists
+	var listHeadB *Node
+	listHead = insertNode(listHead, 7)
+	listHead = insertNode(listHead, 1)
+	listHead = insertNode(listHead, 6)
+	printNode(listHead)
+
+
+	listHeadB = insertNode(listHeadB, 5)
+	listHeadB = insertNode(listHeadB, 9)
+	listHeadB = insertNode(listHeadB, 2)
+	printNode(listHeadB)
+
+	printNode(addLists(listHead, listHeadB, 0))
+	*/
 }
